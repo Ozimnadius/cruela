267c13f1b56a593cb9877fb03b3c85b600d9f41f
Mikle Seregin <mikhail-seregin@yandex.ru>
2021-12-16 21:49:32 +0300

    adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,34 @@
 window.addEventListener('load',function (e){
     const videos = new Swiper('.videos__swiper', {
         slidesPerView: 'auto',
-        spaceBetween: 30,
+        spaceBetween: 10,
         // Navigation arrows
         navigation: {
             nextEl: '.videos__next',
             prevEl: '.videos__prev',
         },
+        breakpoints: {
+            // when window width is >= 768px
+            768: {
+                spaceBetween: 30,
+            }
+        }
     });
 
     const imgs = new Swiper('.imgs__swiper', {
         slidesPerView: 'auto',
-        spaceBetween: 30,
+        spaceBetween: 10,
         // Navigation arrows
         navigation: {
             nextEl: '.imgs__next',
             prevEl: '.imgs__prev',
         },
+        breakpoints: {
+            // when window width is >= 768px
+            768: {
+                spaceBetween: 30,
+            }
+        }
     });
 
 });
