70024d1b90599921109fab8169bdfb8a1d2bac89
Mikle Seregin <mikhail-seregin@yandex.ru>
2021-12-17 10:58:19 +0300

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-window.addEventListener('load',function (e){
+window.addEventListener('load', function (e) {
 
     const videos = new Swiper('.videos__swiper', {
         slidesPerView: 'auto',
@@ -32,17 +32,57 @@ window.addEventListener('load',function (e){
         }
     });
 
-    $('.password__look').on('click', function (e){
-        e.preventDefault();
-
-        if($(this).hasClass('open')){
-            $('.password__val').attr("type", "password");
-            $(this).removeClass('open');
-        } else{
-            $('.password__val').attr("type", "text");
-            $(this).addClass('open');
-        }
-
-    });
+    if (document.querySelector(".password")) {
+        new Password();
+    }
 
 });
+
+class Password {
+    constructor() {
+        this.form = document.querySelector(".password__form");
+        this.letters = document.querySelectorAll(".password__letter");
+        this.look = document.querySelector(".password__look");
+        this.input = document.querySelector(".password__val");
+        this.btn = document.querySelector(".password__submit");
+        this.password = "";
+        this.init();
+    }
+
+    init() {
+        $('.password__look').on('click', function (e) {
+            e.preventDefault();
+
+            if ($(this).hasClass('open')) {
+                $('.password__letter').addClass("hide");
+                $(this).removeClass('open');
+            } else {
+                $('.password__letter').removeClass("hide");
+                $(this).addClass('open');
+            }
+        });
+
+        this.form.addEventListener('input', (e) => {
+
+            let next = e.target.nextElementSibling;
+            this.password = this.password + e.data;
+            e.target.disabled = true;
+            e.target.classList.add("hide");
+
+            if (next) {
+                this.focus(next);
+            } else {
+                this.btn.classList.add("active");
+                this.input.value = this.password;
+                this.btn.focus();
+            }
+
+        });
+    }
+
+    focus(el) {
+        el.focus();
+        el.select();
+    }
+
+}
